e31b6723544e31da49c2cabf594f951f9fc7f813
malada <gam3542@hotmail.com>
2020-03-22 22:04:25 +0700

    login/register layout

--- app/src/main/java/com/example/androidapp/MainActivity.java
+++ app/src/main/java/com/example/androidapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.androidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void startActivities(Intent intent) {
+    }
+
+    public void loginClick(View view) {
+        Intent intent = new Intent(this,LoginActivity.class);
+        startActivity(intent);
+    }
+
+
+    public void signupClick(View view) {
+        Intent intent = new Intent(this,SignupActivity.class);
+        startActivity(intent);
     }
 }
