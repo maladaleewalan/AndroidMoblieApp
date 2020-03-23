a70a0e9a6a1d00661fa8f69641bcc83f965f8f84
malada <gam3542@hotmail.com>
2020-03-23 21:13:42 +0700

    upload profile picture

---
+++ app/src/main/java/com/example/androidapp/PicProfileActivity.java
@@ -0,0 +1,40 @@
+package com.example.androidapp;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
+public class PicProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pic_profile);
+    }
+
+    public void uploadClick(View view) {
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(PicProfileActivity.this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},200);
+        }
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent,0);
+        return;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+         Uri uri = data.getData();
+        ImageView imageView = (ImageView) findViewById((R.id.profilepic));
+        imageView.setImageURI(uri);
+    }
+}
--- app/src/main/java/com/example/androidapp/SignupActivity.java
+++ app/src/main/java/com/example/androidapp/SignupActivity.java
@@ -2,7 +2,9 @@ package com.example.androidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+    }
+
+    public void nextClick(View view) {
+        Intent intent = new Intent(this,PicProfileActivity.class);
+        startActivity(intent);
     }
 }
